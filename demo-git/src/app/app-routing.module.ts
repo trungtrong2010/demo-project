abcb4044882733c37327336c4a6949e0c1a52340
Your Name <you@example.com>
2021-12-21 15:19:35 +0700

    tạo xung đột VTT

--- demo-git/src/app/app-routing.module.ts
+++ demo-git/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CustomerListComponent} from './customer/customer-list/customer-list.component';
+import {CustomerDetailComponent} from './customer/customer-detail/customer-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list', component: CustomerListComponent},
+  {path: 'detail', component: CustomerDetailComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
